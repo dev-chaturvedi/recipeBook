cf73326004f8aa7731b297e0afea72610ef3ad5e
Abhay Chaturvedi <abhaychaturvedi2312@gmail.com>
2025-06-02 21:13:28 +0530

    add a webpack

---
+++ client/webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  mode: 'development',
+  devServer: {
+    static: './dist',
+    historyApiFallback: true,
+    port: 3000
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html'
+    })
+  ]
+};
